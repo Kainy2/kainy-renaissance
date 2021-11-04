80e8748b1b927f6b74990ec82b2c9626e9c64cca
Kainy2 <iartkhay@gmail.com>
2021-11-04 10:31:50 +0100

    lastest

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,7 +14,8 @@ function Main() {
                         <p className='font-semibold -ml-4 text-xl'>All Users</p>
                         <p className='leading-8'>Total ({totalUsers})</p>
                     </div>
-                    <div> <button className='flex px-6 py-3 rounded-md text-white bg-blue-500'> <span className='-my-1 mx-2'> Filter </span> <IoIosArrowDown /> </button> </div>
+                    <div className='mr-24'> <button className='flex px-6 py-3 rounded-md text-white bg-blue-500'> <span className='-my-1 mx-2'> Filter </span> <IoIosArrowDown /> </button> </div>
+                    
                 </div>
 
             </header>
